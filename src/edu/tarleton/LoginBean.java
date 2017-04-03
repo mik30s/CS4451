8048e993336864fa57ca3aeb8d156559d704cbae
michael osei <mosei27@live.com>
2017-04-03 14:54:35 -0500

    Finished login page design

--- src/edu/tarleton/LoginBean.java
+++ src/edu/tarleton/LoginBean.java
@@ -1,5 +1,11 @@
 package edu.tarleton;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
 public class LoginBean {
     private String email;
     private String password;
@@ -18,5 +24,25 @@ public class LoginBean {
     
     public void setPassword(String pass){
         this.password = pass;
+    }
+    
+    public String logIn(){
+        // check if users file exists
+        File userFile = new File(this.email);
+        if(userFile.exists()){
+            try{
+                FileInputStream fis = new FileInputStream(userFile);
+                BufferedReader bf =  new BufferedReader(new InputStreamReader(fis));
+                String password = bf.readLine();
+                if(password.equals(this.password)){
+                      return "menu_page";
+                }
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        
+        return "failed_login";
     }
 }
