66e2d82a555145cde7a4f39c7fce55038d5c4404
michael osei <mosei27@live.com>
2017-04-05 20:04:12 -0500

    Add animation to admin page

--- src/edu/tarleton/ProfileBean.java
+++ src/edu/tarleton/ProfileBean.java
@@ -3,6 +3,14 @@ package edu.tarleton;
 public class ProfileBean {
     User user;
     
+    public User getUser(){
+        return this.user;
+    }
+    
+    public void setUser(User user){
+        this.user = user;
+    }
+    
     public void getUserProfile(){
         
     }
