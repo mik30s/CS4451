bcf79e1c9ca521de07b26b4233985d9616bb1f1c
michael osei <mosei27@live.com>
2017-04-24 12:49:53 -0500

    Added parsley scripts

--- WebContent/resources/js/technician.js
+++ WebContent/resources/js/technician.js
@@ -1,9 +1,27 @@
-var vueMultiSelect = require('vue-multiselect');
 var $ = require('jquery');
 
 module.exports = function(mountId){
 	var vueInstance = new Vue({
 		
+	});
+	
+	var dailyAMModel = {
+		id: "", 
+		name: "",
+		feedEaten: "",
+		feedRefused: "",
+	};
+	
+	$("#am-tab-form").parsley().on('field:validated', function(){
+		var ok = $('.parsley-error').length === 0;
+		if(ok === true){
+			console.log("valid");
+		}
+		else{
+			console.log("not valid");
+		}
+	}).on('form:submit', function(){
+		alert('submiting form are you sure.');
 	});
 
 	return vueInstance;
