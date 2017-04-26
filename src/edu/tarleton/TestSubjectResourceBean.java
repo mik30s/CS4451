1bd784b7b44a56ba03d6277a7abdd328b9300fea
Michael Osei <mosei27@live.com>
2017-04-26 10:53:31 -0500

    Added validation for weekly fields

--- src/edu/tarleton/TestSubjectResourceBean.java
+++ src/edu/tarleton/TestSubjectResourceBean.java
@@ -19,6 +19,8 @@ import javax.ws.rs.core.Response;
 
 @Path("/subject")
 public class TestSubjectResourceBean {
+	public final String FAILED = "failed";
+	public final String SUCCESS = "success";
     public String rootFolderPath =  "C:\\tmp\\course-project\\testsubjectdata\\";
     
     // Returns a subjects data from file
@@ -66,15 +68,18 @@ public class TestSubjectResourceBean {
     @Path("/weekly/add")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response updateWeeklyRecords(@Valid List<Weekly> records) throws Exception {
-        if(records == null || records.size() < 1){
-            return Response.status(201).entity("failed").build();
+        String retval = FAILED;
+    	if(records == null || records.size() < 1){
+            return Response.status(201).entity(retval).build();
         }
         
         for(Weekly record : records){
             System.out.println(record.id + " " + record.name);
+            retval = validateFields(record) ? SUCCESS : FAILED ;
         }
+    
         System.out.println("Updating record");
-        return Response.status(200).entity("success").build();
+        return Response.status(200).entity(retval).build();
     }
     
     @GET
@@ -98,5 +103,22 @@ public class TestSubjectResourceBean {
         }
         
         return Response.status(200).entity("success").build();
+    }
+    
+    public boolean validateFields(Weekly weekly){
+    	boolean isValid = false;
+    	
+    	if(weekly != null) {
+    		if ((weekly.IOP >= 5.0f && weekly.IOP <= 65.0f) && 
+    			(weekly.weight >= 0 && weekly.weight <= 100)) 
+    		{
+    			System.out.println("fields are valid.");
+    			isValid = true;
+    		}
+    		
+    		//System.out.println("fields are not valid.");
+    	}
+    	
+    	return  isValid;
     }
 }
