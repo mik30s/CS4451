91bbe2c0968d90a2e8c86ad2cacdaa5267516d20
michael osei <mosei27@live.com>
2017-04-26 16:28:21 -0500

    Add 20 test subjects to Trash.java

--- src/edu/tarleton/Trash.java
+++ src/edu/tarleton/Trash.java
@@ -17,48 +17,31 @@ public class Trash {
                 new User("mosei27@live.com", "Administrator", "99", "Michael Osei")
         };
         
-        // first subject
-        TestSubject bridget = new TestSubject(42,TestSubject.Group.CONTROL, "Henrietta", TestSubject.Sex.FEMALE);
-        {
-            bridget.notes = "The healthy pig from sesame street";
-            Daily s1DailyAM = new Daily(Period.AM, 0, 0, 0, 0, 0);
-            Daily s1DailyPM = new Daily(Period.PM, 0, 0, 0, 0, 0);
-            bridget.dailyAM = (s1DailyAM);
-            bridget.dailyPM = (s1DailyPM);
-            bridget.weekly = (new Weekly());
-        }      
-        // second subject
-        TestSubject henrietta = new TestSubject(43,TestSubject.Group.TEST, "Bridget", TestSubject.Sex.FEMALE);
-        {
-            henrietta.notes = "That funny pig from the wolf among us";
-            Daily s2DailyAM = new Daily(Period.AM, 0, 0, 0, 0, 0);
-            Daily s2DailyPM = new Daily(Period.PM, 0, 0, 0, 0, 0);
-            henrietta.dailyAM = s2DailyAM;
-            henrietta.dailyPM = s2DailyPM;
-            henrietta.weekly = new Weekly();
-        }
-     // second subject
-        TestSubject wally = new TestSubject(44,TestSubject.Group.TEST, "Wally", TestSubject.Sex.MALE);
-        {
-            wally.notes = "That funny pig from the wolf among us";
-            Daily s2DailyAM = new Daily(Period.AM, 0, 0, 0, 0, 0);
-            Daily s2DailyPM = new Daily(Period.PM, 0, 0, 0, 0, 0);
-            wally.dailyAM = s2DailyAM;
-            wally.dailyPM = s2DailyPM;
-            wally.weekly = new Weekly();
-        }
         
-     // second subject
-        TestSubject paddy = new TestSubject(45,TestSubject.Group.TEST, "Paddy", TestSubject.Sex.MALE);
-        {
-            paddy.notes = "That funny pig from the wolf among us";
-            Daily s2DailyAM = new Daily(Period.AM, 0, 0, 0, 0, 0);
-            Daily s2DailyPM = new Daily(Period.PM, 0, 0, 0, 0, 0);
-            paddy.dailyAM = s2DailyAM;
-            paddy.dailyPM = s2DailyPM;
-            paddy.weekly = new Weekly();
-        }
-        TestSubject[] subs = new TestSubject[]{wally, bridget, henrietta, paddy};
+        TestSubject[] subs = new TestSubject[]{
+            new TestSubject(44,TestSubject.Group.TEST, "Wally", TestSubject.Sex.MALE), 
+            new TestSubject(52,TestSubject.Group.TEST, "Betty", TestSubject.Sex.MALE),
+            new TestSubject(51,TestSubject.Group.TEST, "Bacon", TestSubject.Sex.MALE), 
+            new TestSubject(50,TestSubject.Group.TEST, "Amber", TestSubject.Sex.MALE), 
+            new TestSubject(49,TestSubject.Group.TEST, "Pugsly", TestSubject.Sex.MALE), 
+            new TestSubject(48,TestSubject.Group.TEST, "Polly", TestSubject.Sex.MALE), 
+            new TestSubject(49,TestSubject.Group.TEST, "Shepard", TestSubject.Sex.MALE), 
+            new TestSubject(53,TestSubject.Group.TEST, "Farley", TestSubject.Sex.MALE), 
+            new TestSubject(54,TestSubject.Group.TEST, "Clarice", TestSubject.Sex.MALE), 
+            new TestSubject(55,TestSubject.Group.TEST, "Charlotte", TestSubject.Sex.MALE),
+            new TestSubject(56,TestSubject.Group.TEST, "Hannah", TestSubject.Sex.MALE),
+            new TestSubject(57,TestSubject.Group.TEST, "Jolly", TestSubject.Sex.MALE),
+            new TestSubject(58,TestSubject.Group.TEST, "Lemon", TestSubject.Sex.MALE),
+            new TestSubject(59,TestSubject.Group.TEST, "Piggy", TestSubject.Sex.MALE),
+            new TestSubject(60,TestSubject.Group.TEST, "Ringo", TestSubject.Sex.MALE),
+            new TestSubject(61,TestSubject.Group.TEST, "Bigbone", TestSubject.Sex.MALE),
+            new TestSubject(62,TestSubject.Group.TEST, "Tyson", TestSubject.Sex.MALE),
+            new TestSubject(63,TestSubject.Group.TEST, "Scarlet", TestSubject.Sex.MALE),
+            new TestSubject(64,TestSubject.Group.TEST, "Rose", TestSubject.Sex.MALE),
+            new TestSubject(65,TestSubject.Group.TEST, "Pinky", TestSubject.Sex.MALE),
+            new TestSubject(66,TestSubject.Group.TEST, "Ziggy", TestSubject.Sex.MALE),
+            new TestSubject(67,TestSubject.Group.TEST, "Zoey", TestSubject.Sex.MALE),
+        };
 
         for(User user : users) {
         	File file = new File("C:\\tmp\\course-project\\userdata\\"+user.getEmail());
